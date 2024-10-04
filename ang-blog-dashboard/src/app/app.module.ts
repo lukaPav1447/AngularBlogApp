5cd61742b2bca2f45dcd035c40a2380485ebcc38
pavlovicl <pavlovicl@PAVLOVICL-LAP>
2024-10-04 15:17:11 +0200

    firebase imported and dashboard dizajn uradjen

--- ang-blog-dashboard/src/app/app.module.ts
+++ ang-blog-dashboard/src/app/app.module.ts
@@ -3,16 +3,76 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import {
+  initializeAppCheck,
+  ReCaptchaEnterpriseProvider,
+  provideAppCheck,
+} from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import {
+  getRemoteConfig,
+  provideRemoteConfig,
+} from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'ang-blog-66167',
+        appId: '1:677554543993:web:5157f52974da29fe57a3a8',
+        storageBucket: 'ang-blog-66167.appspot.com',
+        apiKey: 'AIzaSyD79WbtX5MUtFKKMD_RYTpJ3tyYDOwQjb8',
+        authDomain: 'ang-blog-66167.firebaseapp.com',
+        messagingSenderId: '677554543993',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideAppCheck(() => {
+      // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+      const provider = new ReCaptchaEnterpriseProvider(
+        'AIzaSyD79WbtX5MUtFKKMD_RYTpJ3tyYDOwQjb8'
+      );
+      return initializeAppCheck(undefined, {
+        provider,
+        isTokenAutoRefreshEnabled: true,
+      });
+    }),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideVertexAI(() => getVertexAI()),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ ang-blog-dashboard/src/app/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD79WbtX5MUtFKKMD_RYTpJ3tyYDOwQjb8',
+    authDomain: 'ang-blog-66167.firebaseapp.com',
+    projectId: 'ang-blog-66167',
+    storageBucket: 'ang-blog-66167.appspot.com',
+    messagingSenderId: '677554543993',
+    appId: '1:677554543993:web:5157f52974da29fe57a3a8',
+  },
+};
